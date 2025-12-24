f251f8e17d1be0a00f80e2e5a79d012a052ae373
Jahanzaib Ashfaq <jahanzaib@digitalboxes.net>
2025-12-24 15:09:07 +0500

    Disable JazzyWidget on /jazzy-chat to prevent duplicate launcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-
 import JazzyWidget from "../components/JazzyWidget";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // Don't render launcher on iframe chat page
+  const isJazzyChatPage = router.pathname === "/jazzy-chat";
+
   return (
     <>
       <Component {...pageProps} />
-      {/* Global AI Chat Widget */}
-      <JazzyWidget />
+      {!isJazzyChatPage && <JazzyWidget />}
     </>
   );
 }
